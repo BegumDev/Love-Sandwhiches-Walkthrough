f31a81007227aafa75833b68c4faf6e3e322c296
BegumDev <ruk90@outlook.com>
2022-05-21 18:59:25 +0000

    Add function to validate users input is 6 values exactly

--- run.py
+++ run.py
@@ -24,6 +24,20 @@ def get_sales_data():
 
     data_str = input("Enter your data here ")
 
-    print(f"The data you provided is {data_str}")
+    sales_data = data_str.split(",")
+
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Ensure the users input is 6 values exactly
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 sales required, you entered {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")        
 
 get_sales_data()
